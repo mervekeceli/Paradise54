2c412a0a4a1336c07395ce6177a07408334a507e
mervekeceli <mervekeceli55e5@gmail.com>
2022-05-16 13:50:28 +0300

    tablo kaldırıldı

---
+++ DataAccessLayer/Migrations/20220516104922_addStock.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class addStock : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Drinks_DrinkId",
+                table: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "Menus");
+
+            migrationBuilder.DropTable(
+                name: "Drinks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CartItems_DrinkId",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "DrinkId",
+                table: "CartItems");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Stock",
+                table: "Foods",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Stock",
+                table: "Foods");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DrinkId",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Drinks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Photo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drinks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    DrinkId = table.Column<int>(type: "int", nullable: false),
+                    FoodId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Menus_Drinks_DrinkId",
+                        column: x => x.DrinkId,
+                        principalTable: "Drinks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Menus_Foods_FoodId",
+                        column: x => x.FoodId,
+                        principalTable: "Foods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_DrinkId",
+                table: "CartItems",
+                column: "DrinkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Menus_DrinkId",
+                table: "Menus",
+                column: "DrinkId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Menus_FoodId",
+                table: "Menus",
+                column: "FoodId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Drinks_DrinkId",
+                table: "CartItems",
+                column: "DrinkId",
+                principalTable: "Drinks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
